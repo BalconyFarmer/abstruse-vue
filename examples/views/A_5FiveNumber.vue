28e34958286fcdd4ed910af130f82d8978bc9eb8
wangkewei1993 <1067647932@qq.com>
2021-08-04 16:34:04 +0800

    1

--- examples/views/A_5FiveNumber.vue
+++ examples/views/A_5FiveNumber.vue
@@ -1,9 +1,8 @@
 <template>
-
     <div class="all">
         <div class="allTop">
             <div style="width: 400px;height: 400px;">
-                <fiveNumber></fiveNumber>
+                <fiveNumber :fData=fData></fiveNumber>
             </div>
         </div>
         <div class="allBottom">
@@ -14,19 +13,21 @@
 
 <script>
 
-// import fiveNumber from "../packages/abstruse-vue/vueConponents/fiveNumber/fiveNumber";
-import {fiveNumber} from "../../lib/index.common.js";
+import fiveNumber from "../../packages/abstruse-vue/vueConponents/fiveNumber/fiveNumber";
+// import {fiveNumber} from "../../lib/index.common.js";
 
 import CodeBlock from "./commonComponents/CodeBlock";
 
 export default {
     name: "A_5FiveNumber",
     components: {
-        fiveNumber,CodeBlock
+        fiveNumber, CodeBlock
     },
     data() {
         return {
-            code: "<fiveNumber></fiveNumber>"
+            code: "<fiveNumber :fData = fData></fiveNumber>\n " +
+                "fData: [1,2,3,4,5]",
+            fData: [1, 2, 3, 4, 5]
         }
     },
 }
--- packages/abstruse-vue/vueConponents/fiveNumber/fiveNumber.vue
+++ packages/abstruse-vue/vueConponents/fiveNumber/fiveNumber.vue
@@ -5,35 +5,35 @@
         <div class="org-item org-item-1">
             <div class="img-box">
                 <img src="./imgs/ic_org_c.png" alt="">
-                <div class="item-label">{{ 11 + "%" }}</div>
+                <div class="item-label">{{ fData[0] + "%" }}</div>
             </div>
             <p>离退休</p>
         </div>
         <div class="org-item org-item-2">
             <div class="img-box">
                 <img class="routeImg" src="./imgs/ic_org_c.png" alt="">
-                <div class="item-label">{{ 11 + "%" }}</div>
+                <div class="item-label">{{ fData[1] + "%" }}</div>
             </div>
             <p>男性</p>
         </div>
         <div class="org-item org-item-3">
             <div class="img-box">
                 <img class="routeImg" src="./imgs/ic_org_c.png" alt="">
-                <div class="item-label">{{ 11 + "%" }}</div>
+                <div class="item-label">{{ fData[2] + "%" }}</div>
             </div>
             <p>本科以上</p>
         </div>
         <div class="org-item org-item-4">
             <div class="img-box">
                 <img class="routeImg" src="./imgs/ic_org_c.png" alt="">
-                <div class="item-label">{{ 11 + "%" }}</div>
+                <div class="item-label">{{ fData[3] + "%" }}</div>
             </div>
             <p>少数民族</p>
         </div>
         <div class="org-item org-item-5">
             <div class="img-box">
                 <img class="routeImg" src="./imgs/ic_org_c.png" alt="">
-                <div class="item-label">{{ 11 + "%" }}</div>
+                <div class="item-label">{{ fData[4] + "%" }}</div>
             </div>
             <p>资深老党员</p>
         </div>
@@ -42,7 +42,9 @@
 
 <script>
 export default {
-    name: "fiveNumber"
+    name: "fiveNumber",
+    props: ["fData"],
+
 }
 </script>
 
