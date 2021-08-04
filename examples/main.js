d34d8bc2bf90b017a16a89aea6d55a9e6cb67c0a
wangkewei1993 <1067647932@qq.com>
2021-08-04 09:41:58 +0800

    1

--- examples/main.js
+++ examples/main.js
@@ -7,6 +7,11 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import "./views/styles/base.scss"
 
+import { PrismEditor } from 'vue-prism-editor';
+import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles
+Vue.component('PrismEditor', PrismEditor);
+
+
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
--- examples/views/A_1PictureTree.vue
+++ examples/views/A_1PictureTree.vue
@@ -10,15 +10,19 @@
             </div>
         </div>
         <div class="allBottom">
-            {{str}}
+            <prism-editor class="my-editor" v-model="code" :highlight="highlighter" line-numbers></prism-editor>
         </div>
     </div>
 </template>
 
 <script>
-
 // import WaterBall from "../packages/abstruse-vue/vueConponents/WaterBall/waterBall";
 import {WaterBall} from "../../lib/index.common.js";
+
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
 
 export default {
     name: "A_1PictureTree.vue",
@@ -27,10 +31,15 @@ export default {
     },
     data() {
         return {
-            str: "<WaterBall :data=\"0.7\" :special=\"true\"></WaterBall>" +
-                "<WaterBall :data=\"0.7\" :special=\"false\"></WaterBall>"
+            code: "<WaterBall :data=\"0.7\" :special=\"true\"></WaterBall>\n" +
+                  "<WaterBall :data=\"0.7\" :special=\"false\"></WaterBall> ",
         }
-    }
+    },
+    methods: {
+        highlighter(code) {
+            return highlight(code, languages.js); // languages.<insert language> to return html with markup
+        },
+    },
 }
 </script>
 
